d61b66a59b2ce324dc14b5f12f5617998c27d969
Phezzalicious <starfish206@gmail.com>
2019-12-06 20:19:03 -0600

    Bill UpDATE

--- Bill.cs
+++ Bill.cs
@@ -10,8 +10,8 @@ namespace oop_assign_5
         double amount;
         List<Payment> payments;
         public Bill(string date, string description,double amount){
-            billDate =date;
-            this.description =description;
+            billDate = date;
+            this.description = description;
             this.amount = amount;
             payments = new List<Payment>();
 
@@ -20,16 +20,34 @@ namespace oop_assign_5
             string paymentInfo = $"Date: {billDate} \nDescription: {description} \nAmount: {amount}\n";
             foreach(Payment payment in payments){
                 string addToPaymentInfo = $"On {payment.getPaymentDate()} payment with {payment.getPaymentType()} the amount {payment.getAmount()} was processed \n";
-            paymentInfo += addToPaymentInfo;
+                paymentInfo += addToPaymentInfo;
             }
-            
-            
+            paymentInfo += $"Total Paid: {totalPaid()}\nRemaining Balance: {getBalance()}";
             return paymentInfo;
         }
         public bool addPayment(Payment p){
             p.verify();
             payments.Add(p);
             return true;
+        }
+        public double totalPaid()
+        {
+            double total = 0;
+            foreach(Payment payment in payments)
+            {
+            total = total + payment.getAmount(); 
+            }
+            return total;
+        }
+
+        public double getBalance()
+        {
+          double btotal = 0; 
+          foreach(var payment in payments)
+          {
+          	btotal = btotal + payment.getAmount();
+          }
+          return amount - totalPaid();
         }
     }
 }
